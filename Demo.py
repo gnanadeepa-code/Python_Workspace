c85d8e31a29c646291fde54efe8d590131f9dc34
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2025-10-10 12:28:32 +0530

    factorial changes

--- Demo.py
+++ Demo.py
@@ -1,4 +1,4 @@
-# Python 3.x program to check if an array consists 
+""" # Python 3.x program to check if an array consists 
 # of even number
 def contains_even_number(l):
     for ele in l:
@@ -16,3 +16,65 @@ print ("For List 1:")
 contains_even_number([1, 9, 8])
 print (" \nFor List 2:")
 contains_even_number([1, 3, 5])
+
+count = 0
+while (count < 1):    
+    count = count+1
+    print(count)
+    break
+else:
+    print("No Break")
+
+# Print multiplication table of 3
+n = 3
+
+for i in range(1, n + 1):
+    for j in range(1, 11):
+        print(i * j, end=' ')
+    print()
+
+# Function to print powers of 2
+
+def printIncreasingPower(x):
+    #code here
+    print("Powers of 2 are: ",x)
+    num = int(x)
+    i = 1
+    # Loop to jump in powers of 2
+    while(i <= num):
+        #code here
+        y =  i*i
+       # print ("value:",y)
+        if(y<=num):
+           #  print ("y:",y, "i:",i,"num:",num)
+             print(y, end = ' ')
+             i += 1
+        else:
+            break
+
+        #code here
+
+printIncreasingPower(10)
+
+#User function Template for python3
+n = int(input())
+
+# Your code here
+for i in range(1,11):
+    print(n*i,end=' ') 
+
+                       """
+
+class Solution:
+    # Function to calculate factorial of a number.
+    def factorial(self, n: int) -> int:
+        # code here
+        print("n:",n)
+        tot = 1
+        for i in range(1,n+1):
+            tot *= i
+            print("i:",i,"tot:",tot)
+        return tot
+
+ob = Solution()
+print(ob.factorial(5))
